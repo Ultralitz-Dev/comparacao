01a0113ee382b93c83b6aaeca8e3034a065c8c33
João Vitor <129129458+Ultralitz-Dev@users.noreply.github.com>
2023-04-04 22:44:47 -0300

    Add files via upload

---
+++ Comparação.js
@@ -0,0 +1,18 @@
+10 == 10;
+10 != 10;
+10 < 10;
+10 <= 10;
+10 > 10;
+10 >= 10;
+10 < 20;
+
+// Comparação de Textos
+"Felipe" == "Felipe"
+"felipe" == "Felipe"
+"A" < "B" 
+"C" > "D" 
+
+var a = 10
+var b = 20
+
+b > a
